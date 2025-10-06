79321fc63da4af6f2328cd68abdd9c96df0c2031
nicholasA113 <nde1222@byu.edu>
2025-10-06 13:28:32 -0600

    started implementation of react

---
+++ react-version/src/components/story.jsx
@@ -0,0 +1,32 @@
+import './read.css';
+
+export default function MadLibs(){
+    return (
+        <body>
+            <header id="page-guidance">
+                <h1>Mad Libs©</h1>
+                <button onclick="location.href='createstory.html'">Create Story</button>
+                <button onclick="location.href='mystories.html'">My Stories</button>
+                <button onclick="location.href='communityboard.html'">Community Board</button>
+                <button onclick="location.href='about.html'">About</button>
+                <hr>
+            </header>
+            <section>
+                <header id=storyTitle><b><u>Story Title</u></b></header>
+                <p id="username"><i>by [username]</i></p>
+                <p id="storyContent">Lorum Ipsum Dolor</p>
+                <br>
+                <div id="checkbox-area">
+                <label for="checkbox1">Post to Community Board?</label>
+                <input type="checkbox" id="checkbox1" name="varCheckbox1" value="checkbox1"/>
+                <label for="checkbox2">  |  Save Story to Favorites?</label>
+                <input type="checkbox" id="checkbox2" name="varCheckbox2" value="checkbox2"/>
+                </div>
+            </section>
+            <footer>
+                <hr>
+                <a href="https://github.com/nicholasA113/startup">Github</a>
+            </footer>
+        </body>
+    );
+}
