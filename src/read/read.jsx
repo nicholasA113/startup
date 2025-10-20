c8c5879a194ab735a7b400acd58e928c555701f4
Nicholas Erickson <77360369+nicholasA113@users.noreply.github.com>
2025-10-20 14:25:25 -0600

    Update read.jsx

--- src/read/read.jsx
+++ src/read/read.jsx
@@ -3,43 +3,45 @@ import { useNavigate, NavLink } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import './read.css';
 
-export function Read(){
+export function Read() {
     const navigate = useNavigate();
     const storedTempUser = JSON.parse(localStorage.getItem('tempUser'));
 
+    const selectedReadStory = JSON.parse(localStorage.getItem('selectedReadStory'));
     const storyTemplate = localStorage.getItem('storyTemplate');
     const filledWords = JSON.parse(localStorage.getItem('filledWords'));
     const storyTitle = localStorage.getItem('storyTitle');
 
-    const fullStory = storyTemplate.replace(/{{(\d+)}}/g, (_, i) => filledWords[i - 1]);
+    const fullStory = storyTemplate
+        ? storyTemplate.replace(/{{(\d+)}}/g, (_, i) => filledWords[i - 1])
+        : selectedReadStory?.content;
+
+    const author = selectedReadStory?.author || storedTempUser.username;
+    const title = selectedReadStory?.title || storyTitle;
 
     const handleSaveStory = () => {
         const savedStories = JSON.parse(localStorage.getItem('savedStories')) || [];
-
         const newStory = {
-            title: storyTitle,
+            title,
             content: fullStory,
             author: storedTempUser.username,
         };
-        
         savedStories.push(newStory);
         localStorage.setItem('savedStories', JSON.stringify(savedStories));
         navigate('/mystories');
-    }
+    };
 
     const handleCreateAnother = () => {
         const savedStories = JSON.parse(localStorage.getItem('savedStories')) || [];
-
         const newStory = {
-            title: storyTitle,
+            title,
             content: fullStory,
             author: storedTempUser.username,
         };
-        
         savedStories.push(newStory);
         localStorage.setItem('savedStories', JSON.stringify(savedStories));
         navigate('/createstory');
-    }
+    };
 
     return (
         <main id="read-page">
@@ -52,22 +54,22 @@ export function Read(){
                 <Button className="buttons" onClick={() => navigate('/about')}>About</Button>
                 <hr />
             </header>
-            
+
             <section id="story">
-                <header id="storyTitle"><b><u>{storyTitle}</u></b></header>
-                <p id="username"><i>by {storedTempUser.username}</i></p>
+                <header id="storyTitle"><b><u>{title}</u></b></header>
+                <p id="username"><i>by {author}</i></p>
                 <p id="storyContent">{fullStory}</p>
                 <br />
                 <div id="checkbox-area">
-                    {storedTempUser.username === storyTemplate.author && (
+                    {storedTempUser?.username === author && (
                         <>
-                            <label for="checkbox1">Post to Community Board?</label>
-                            <input type="checkbox" id="checkbox1" name="varCheckbox1" value="checkbox1"/>
-                            <p>  |  </p>
+                            <label htmlFor="checkbox1">Post to Community Board?</label>
+                            <input type="checkbox" id="checkbox1" name="varCheckbox1" value="checkbox1" />
+                            <p> | </p>
                         </>
                     )}
-                    <label for="checkbox2">Save Story to Favorites?</label>
-                    <input type="checkbox" id="checkbox2" name="varCheckbox2" value="checkbox2"/>
+                    <label htmlFor="checkbox2">Save Story to Favorites?</label>
+                    <input type="checkbox" id="checkbox2" name="varCheckbox2" value="checkbox2" />
                 </div>
                 <br />
                 <br />
@@ -77,7 +79,7 @@ export function Read(){
                     <Button className="buttons" onClick={handleCreateAnother}>Create Another Story</Button>
                 </div>
             </section>
-            
+
             <footer className="footer">
                 <hr />
                 <NavLink className="nav-link" to="https://github.com/nicholasA113/startup">Github</NavLink>
