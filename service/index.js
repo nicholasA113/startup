25edd8265783e36adf52429460d0e00b95418c1b
nicholasA113 <nde1222@byu.edu>
2025-10-29 13:56:52 -0600

    changes

--- service/index.js
+++ service/index.js
@@ -1,3 +1,63 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const express = require('express');
+const uuid = require('uuid');
+const app = express();
+
+const authCookieName = 'token';
+
+let users = [];
+
+let stories = [];
+let favorites = {};
+
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
+app.use(express.json());
+app.use(cookieParser());
 app.use(express.static('public'));
+
+var apiRouter = express.Router();
+app.use(`api`, apiRouter);
+
+const verifyUser = async (req, res, next) => {
+    const user = await findUser('token', req.cookies[authCookieName]);
+    if (user) {
+        next();
+    }
+    else {
+        res.status(401).send({msg: 'Unauthorized'});
+    }
+}
+
+apiRouter.post('/auth/create', async (req, res) => {
+    if (await findUser('username', req.body.username)) {
+        res.status(409).send({msg: 'Existing user'});
+    }
+    else {
+        const user = await createUser(req.body.username, req.body.password);
+        setAuthCookie(res, user.token);
+        res.send({username: user.username});
+    }
+});
+
+apiRouter.post('/auth/login', async(req, res) => {
+    const user = await findUser('username', req.body.username);
+    if (user && await bcrypt.compare(req.body.password, user.password)) {
+        user.token = uuid.v4();
+        setAuthCookie(res, user.token);
+        res.send({username: user.username});
+    }
+    else{
+        res.status(401).send({msg: 'Unauthorized'});
+    }
+});
+
+apiRouter.delete('/auth/logout', async(req, res) => {
+    const user = await findUser('token', req.cookies[authCookieName]);
+    if (user){
+        delete user.token;
+    }
+    res.clearCookie(authCookieName);
+    res.status(204).end();
+})
