6688486d2338fd0acf026597449eee4676070c30
Nicholas Erickson <77360369+nicholasA113@users.noreply.github.com>
2025-11-05 15:39:11 -0700

    Update database.js

--- service/database.js
+++ service/database.js
@@ -1,81 +1,91 @@
 const { MongoClient } = require('mongodb');
 const config = require('./dbConfig.json');
 
-const url = `mogodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
 const db = client.db('madlibs');
+
 const userCollection = db.collection('user');
 const storyCollection = db.collection('story');
 const favoriteCollection = db.collection('favorite');
 
-(async function testconnection() {
-    try {
-        await db.command({ ping: 1});
-        console.log('Connected to database');
-    }
-    catch (ex) {
-        console.error(`Unable to connect to database: ${ex.message}`);
-        process.exit(1);
-    }
+(async function testConnection() {
+  try {
+    await db.command({ ping: 1 });
+    console.log('Connected to database');
+  } catch (ex) {
+    console.error(`Unable to connect to database with ${url}: ${ex.message}`);
+    process.exit(1);
+  }
 })();
 
-async function getUser(username){
-    return userCollection.findOne({username});
+function getUser(username) {
+  return userCollection.findOne({ username: username });
 }
 
-async function getUserByToken(token) {
-    return userCollection.findOne({token});
+function getUserByToken(token) {
+  return userCollection.findOne({ token: token });
 }
 
 async function addUser(user) {
-    return userCollection.insertOne(user);
+  await userCollection.insertOne(user);
 }
 
-async function updateUserToken(username, token){
-    return userCollection.updateOne({username}, { $set: {token}});
+async function updateUser(user) {
+  await userCollection.updateOne({ username: user.username }, { $set: user });
+}
+
+async function updateUserToken(username, token) {
+  await userCollection.updateOne({ username }, { $set: { token } });
 }
 
 async function addStory(story) {
-    return storyCollection.insertOne(story);
+  return storyCollection.insertOne(story);
 }
 
-async function getUserStories(username) {
-    return storyCollection.find({author: username}).toArray();
+function getUserStories(username) {
+  return storyCollection.find({ author: username }).toArray();
 }
 
-async function getCommunityStories() {
-    return storyCollection.find({ postToCommunity: true}).toArray();
+function getCommunityStories() {
+  return storyCollection.find({ postToCommunity: true }).toArray();
+}
+
+function getStory(id) {
+  return storyCollection.findOne({ id: id });
 }
 
 async function updateStory(id, updates) {
-    return storyCollection.updateOne({id}, {$set: updates});
+  return storyCollection.updateOne({ id: id }, { $set: updates });
 }
 
 async function toggleFavorite(username, storyId) {
-    const fav = await favoriteCollection.findOne({username, storyId});
-    if (fav) {
-        return favoriteCollection.deleteOne({username, storyId});
-    }
-    else {
-        return favoriteCollection.insertOne({username, storyID});
-    }
+  const existing = await favoriteCollection.findOne({ username, storyId });
+  if (existing) {
+    return favoriteCollection.deleteOne({ username, storyId });
+  } 
+  else {
+    return favoriteCollection.insertOne({ username, storyId });
+  }
 }
 
 async function getFavorites(username) {
-    const favorites = await favoriteCollection.find({username}).toArray();
-    const storyIds = favorites.map( f => f.storyId);
-    return storyCollection.find({ id: {$in: storyIds}}).toArray();
+  const favorites = await favoriteCollection.find({ username }).toArray();
+  const storyIds = favorites.map(f => f.storyId);
+  return storyCollection.find({ id: { $in: storyIds } }).toArray();
 }
 
 module.exports = {
-    getUser,
-    addUser,
-    getUserByToken,
-    updateUserToken,
-    addStory,
-    getUserStories,
-    getCommunityStories,
-    updateStory,
-    toggleFavorite,
-    getFavorites,
+  getUser,
+  getUserByToken,
+  addUser,
+  updateUser,
+  updateUserToken,
+  addStory,
+  getStory,
+  getUserStories,
+  getCommunityStories,
+  updateStory,
+  toggleFavorite,
+  getFavorites,
 };
